55b5a0d69444a75660eee06e38dcf89f1969373e
Jelle Mattez <info@blackweb.be>
2010-11-21 13:38:41 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8a12ad6b7e043c274bf589648a650b9d9bddaffb4627b20655abade29456f9a327364338fac53eb2c80657eb83d49ee4b21f27dcaa74dd27e6146420653faa2e'
